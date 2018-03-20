f176a688761002c215915a7a7d7f32672f08b52c
Kamil Ismail <ismail.katowice@gmail.com>
2018-03-20 21:55:08 +0100

    Dalsze prace nad 1 pomieszczeniem

---
+++ Escape Room/Assets/Scripts/CameraController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	// Use this for initialization
+	public float sensitivity = 5;
+	public float smooth = 2;
+	public float min = -40;
+	public float max = 60;
+
+	Vector2 mouseLook;
+	Vector2 smoothVector;
+	GameObject player;
+
+	void Start () {
+		player = this.transform.parent.gameObject;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Vector2 mousePos = new Vector2 (Input.GetAxisRaw ("Mouse X"), Input.GetAxisRaw ("Mouse Y"));
+
+		mousePos = Vector2.Scale (mousePos, new Vector2 (sensitivity*smooth, sensitivity*smooth));
+		smoothVector.x = Mathf.Lerp (smoothVector.x, mousePos.x, 1 / smooth);
+		smoothVector.y = Mathf.Lerp (smoothVector.y, mousePos.y, 1 / smooth);
+
+		//smoothVector.y = Mathf.Clamp (mousePos.y, min, max); //??????
+
+		mouseLook += smoothVector;
+
+		transform.localRotation = Quaternion.AngleAxis (-mouseLook.y, Vector3.right);
+		player.transform.localRotation = Quaternion.AngleAxis (mouseLook.x, player.transform.up);
+	}
+}
---
+++ Escape Room/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	public float speed = 10;
+
+	void Start () {
+		//Cursor.lockState = CursorLockMode.Locked;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float ver = Input.GetAxis ("Vertical") * speed;
+		float hor = Input.GetAxis ("Horizontal") * speed;
+
+		ver *= Time.deltaTime;
+		hor *= Time.deltaTime;
+
+		transform.Translate (hor, 0, ver);
+	}
+}
