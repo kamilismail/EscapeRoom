4fd3095cbbfb9dc4c931c802033f21d9bb32f5a0
jpisula <jakub.pisula@wp.pl>
2018-03-20 23:28:43 +0100

    Dodanie postaci, poruszania się, poprawa pomieszczenia

--- Escape Room/Assets/Scripts/CameraController.cs
+++ Escape Room/Assets/Scripts/CameraController.cs
@@ -9,8 +9,11 @@ public class CameraController : MonoBehaviour {
 	public float smooth = 2;
 	public float min = -40;
 	public float max = 60;
+    public Vector2 mousePos;
+    public GameObject camera;
 
-	Vector2 mouseLook;
+
+    Vector2 mouseLook;
 	Vector2 smoothVector;
 	GameObject player;
 
@@ -20,17 +23,54 @@ public class CameraController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		Vector2 mousePos = new Vector2 (Input.GetAxisRaw ("Mouse X"), Input.GetAxisRaw ("Mouse Y"));
+        if (transform.rotation.eulerAngles.x >= 310 && transform.rotation.eulerAngles.x <= 360)
+        {
+            //max = 1;
+            ////transform.Rotate(0, 0, 0);
+            //camera.transform.rotation.eulerAngles.Set(0, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
+            mousePos = new Vector3(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+            mousePos = Vector2.Scale(mousePos, new Vector2(sensitivity * smooth, sensitivity * smooth));
+            smoothVector.x = Mathf.Lerp(smoothVector.x, mousePos.x, 1 / smooth);
+            smoothVector.y = Mathf.Lerp(smoothVector.y, mousePos.y, 1 / smooth);
+            //smoothVector.y = Mathf.Clamp (mousePos.y, min, max); //??????
 
-		mousePos = Vector2.Scale (mousePos, new Vector2 (sensitivity*smooth, sensitivity*smooth));
-		smoothVector.x = Mathf.Lerp (smoothVector.x, mousePos.x, 1 / smooth);
-		smoothVector.y = Mathf.Lerp (smoothVector.y, mousePos.y, 1 / smooth);
+            mouseLook += smoothVector;
 
-		//smoothVector.y = Mathf.Clamp (mousePos.y, min, max); //??????
+            transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
+            player.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, player.transform.up);
 
-		mouseLook += smoothVector;
+            if (transform.rotation.eulerAngles.x < 310 && transform.rotation.eulerAngles.x > 50)
+            {
+                transform.rotation = Quaternion.Euler(311, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
+                
+            }
 
-		transform.localRotation = Quaternion.AngleAxis (-mouseLook.y, Vector3.right);
-		player.transform.localRotation = Quaternion.AngleAxis (mouseLook.x, player.transform.up);
-	}
+        }
+        else if (transform.rotation.eulerAngles.x <= 45 && transform.rotation.eulerAngles.x >= 0)
+        {
+            //transform.Rotate(max, 0, 0);
+            //transform.rotation.eulerAngles.Set(44.9f, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
+            mousePos = new Vector3(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+            mousePos = Vector2.Scale(mousePos, new Vector2(sensitivity * smooth, sensitivity * smooth));
+            smoothVector.x = Mathf.Lerp(smoothVector.x, mousePos.x, 1 / smooth);
+            smoothVector.y = Mathf.Lerp(smoothVector.y, mousePos.y, 1 / smooth);
+            //smoothVector.y = Mathf.Clamp (mousePos.y, min, max); //??????
+
+            mouseLook += smoothVector;
+
+            transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
+            player.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, player.transform.up);
+
+            if (transform.rotation.eulerAngles.x > 45 && transform.rotation.eulerAngles.x < 300)
+            {
+                transform.rotation = Quaternion.Euler(44, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
+
+            }
+        }
+        min = transform.rotation.eulerAngles.x;
+        
+            //transform.rotation.eulerAngles.Set(310, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
+
+
+    }
 }
