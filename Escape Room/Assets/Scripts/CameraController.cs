348c35f5029aa1710712948745a8d9bb0c59fc33
Kamil Ismail <ismail.katowice@gmail.com>
2018-03-21 20:23:30 +0100

    Menu pauzy, poprawiona kamera

--- Escape Room/Assets/Scripts/CameraController.cs
+++ Escape Room/Assets/Scripts/CameraController.cs
@@ -7,8 +7,8 @@ public class CameraController : MonoBehaviour {
 	// Use this for initialization
 	public float sensitivity = 5;
 	public float smooth = 2;
-	public float min = -40;
-	public float max = 60;
+	public float min = -45f;
+	public float max = 50f;
     public Vector2 mousePos;
     public GameObject camera;
 
@@ -23,54 +23,14 @@ public class CameraController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-        if (transform.rotation.eulerAngles.x >= 310 && transform.rotation.eulerAngles.x <= 360)
-        {
-            //max = 1;
-            ////transform.Rotate(0, 0, 0);
-            //camera.transform.rotation.eulerAngles.Set(0, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
-            mousePos = new Vector3(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
-            mousePos = Vector2.Scale(mousePos, new Vector2(sensitivity * smooth, sensitivity * smooth));
-            smoothVector.x = Mathf.Lerp(smoothVector.x, mousePos.x, 1 / smooth);
-            smoothVector.y = Mathf.Lerp(smoothVector.y, mousePos.y, 1 / smooth);
-            //smoothVector.y = Mathf.Clamp (mousePos.y, min, max); //??????
 
-            mouseLook += smoothVector;
-
-            transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
-            player.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, player.transform.up);
-
-            if (transform.rotation.eulerAngles.x < 310 && transform.rotation.eulerAngles.x > 50)
-            {
-                transform.rotation = Quaternion.Euler(311, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
-                
-            }
-
-        }
-        else if (transform.rotation.eulerAngles.x <= 45 && transform.rotation.eulerAngles.x >= 0)
-        {
-            //transform.Rotate(max, 0, 0);
-            //transform.rotation.eulerAngles.Set(44.9f, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
-            mousePos = new Vector3(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
-            mousePos = Vector2.Scale(mousePos, new Vector2(sensitivity * smooth, sensitivity * smooth));
-            smoothVector.x = Mathf.Lerp(smoothVector.x, mousePos.x, 1 / smooth);
-            smoothVector.y = Mathf.Lerp(smoothVector.y, mousePos.y, 1 / smooth);
-            //smoothVector.y = Mathf.Clamp (mousePos.y, min, max); //??????
-
-            mouseLook += smoothVector;
-
-            transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
-            player.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, player.transform.up);
-
-            if (transform.rotation.eulerAngles.x > 45 && transform.rotation.eulerAngles.x < 300)
-            {
-                transform.rotation = Quaternion.Euler(44, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
-
-            }
-        }
-        min = transform.rotation.eulerAngles.x;
-        
-            //transform.rotation.eulerAngles.Set(310, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
-
-
+		mousePos = new Vector3(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+		mousePos = Vector2.Scale(mousePos, new Vector2(sensitivity * smooth, sensitivity * smooth));
+		smoothVector.x = Mathf.Lerp(smoothVector.x, mousePos.x, 1 / smooth);
+		smoothVector.y = Mathf.Lerp(smoothVector.y, mousePos.y, 1 / smooth);
+		mouseLook += smoothVector;
+		mouseLook.y = Mathf.Clamp(mouseLook.y, min, max);﻿
+		transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
+		player.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, player.transform.up);
     }
 }
---
+++ Escape Room/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+
+	public bool GamePaused = false;
+	public GameObject pauseMenu;
+	public string menuSceneName;
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			if (GamePaused) {
+				Resume ();
+			} else {
+				Pause();
+		}
+	}
+}
+
+	public void Resume() {
+		pauseMenu.SetActive (false);
+		Time.timeScale = 1;
+		GamePaused = false;
+	}
+
+	public void Pause() {
+		pauseMenu.SetActive (true);
+		Time.timeScale = 0;
+		GamePaused = true;
+	}
+
+	public void LoadMainMenu() {
+		menuSceneName = "Menu";
+		Time.timeScale = 1;
+		SceneManager.LoadScene (menuSceneName);
+	}
+
+}
